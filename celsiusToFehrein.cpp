7c9065b87f9311763b2a013cc4d8be34285a885e
Raksha Jaiswal <ku.rakshadilipjaiswal@gmail.com>
2024-02-25 13:50:58 +0530

    celsiusToFeh

---
+++ celsiusToFehrein.cpp
@@ -0,0 +1,12 @@
+//c to f
+//formula => c = (32f - 32) * (5/9)
+//formula => f = (c * (9/5))+32;
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	float f;
+	float c = (f-32)*(5/9);
+	cout<<"The celsiu is : "<<c;
+}
