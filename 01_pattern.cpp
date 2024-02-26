4c92ee33991e76ec4039204ffbe916694180befd
Raksha Jaiswal <ku.rakshadilipjaiswal@gmail.com>
2024-02-26 22:19:24 +0530

    01_pattern

---
+++ 01_pattern.cpp
@@ -0,0 +1,34 @@
+//pattern
+/*
+   1
+   121
+  12321
+ 1234321
+123454321
+*/
+
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        
+        for(int j=n;j>i;j--){
+            cout<<" ";
+        }
+        
+        for(int j=1;j<=i;j++){
+            cout<<j;
+        }
+        
+        for(int j=1;j<i;j++){
+            int val = i-j;
+            cout<<val;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
